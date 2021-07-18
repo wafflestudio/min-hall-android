23dbd519f3de20b1d5cf294a0ffeb8e3eba69d98
Ion Jo <onemonsterceo@gmail.com>
2021-07-18 16:57:40 +0900

    Initial commit

---
+++ app/src/main/java/com/wafflestudio/snucse/minhall/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.wafflestudio.snucse.minhall
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
