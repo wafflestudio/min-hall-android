b369b31ade3699334f3c930e05a8840964e35f26
Ion Jo <onemonsterceo@gmail.com>
2021-07-19 00:49:56 +0900

    Used zoomlayout to implement map

--- app/src/main/java/com/wafflestudio/snucse/minhall/MainActivity.kt
+++ app/src/main/java/com/wafflestudio/snucse/minhall/MainActivity.kt
@@ -1,11 +1,26 @@
 package com.wafflestudio.snucse.minhall
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.wafflestudio.snucse.minhall.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        initializeZoom()
+    }
+
+    private fun initializeZoom() {
+        binding.map.post {
+            val minZoom = binding.zoom.height.toFloat() / binding.map.height / binding.zoom.realZoom
+            binding.zoom.setMaxZoom(minZoom * 2)
+            binding.zoom.setMinZoom(minZoom)
+        }
     }
 }
